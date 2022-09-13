0aa0b9b61faaa068167565ae066705464115aea9
Phils Garcia <eivanphils@gmail.com>
2022-09-13 14:33:12 -0300

    Agregada configuracion para usar el scannerQr

--- lib/widgets/scan_button.dart
+++ lib/widgets/scan_button.dart
@@ -1,14 +1,19 @@
 import 'package:flutter/material.dart';
 
+import 'package:flutter_barcode_scanner/flutter_barcode_scanner.dart';
+
 class ScanButton extends StatelessWidget {
   const ScanButton({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return FloatingActionButton(
-      elevation: 0,
-      child: const Icon(Icons.qr_code),
-      onPressed: () {}
-    );
+        elevation: 0,
+        child: const Icon(Icons.qr_code),
+        onPressed: () async {
+          String barcodeScanRes = await FlutterBarcodeScanner.scanBarcode(
+              '#3D8BEF', 'Cancelar', false, ScanMode.QR);
+          print(barcodeScanRes);
+        });
   }
 }
